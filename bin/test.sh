5233b978e6858b1ef9d4148a65b560cecf17a9ff
imura <imura@hitomedia.jp>
2015-02-23 16:19:14 +0900

    Modified test.sh

--- bin/test.sh
+++ bin/test.sh
@@ -1,3 +1,5 @@
+#!/bin/sh -ex
 vendor/bin/phpunit -c ./ --coverage-html=$CIRCLE_ARTIFACTS/coverage --log-junit=$CIRCLE_TEST_REPORTS/junit.xml src/App/Test/*Test.php
 mkdir $CIRCLE_ARTIFACTS/phpmd
 vendor/bin/phpmd src html cleancode,codesize,controversial,design,naming,unusedcode --reportfile $CIRCLE_ARTIFACTS/phpmd/phpmd.html
+exit 1
