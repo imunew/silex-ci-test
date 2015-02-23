72225a59b2738235681ec724a85a197066c8abcd
imura <imura@hitomedia.jp>
2015-02-23 16:03:15 +0900

    Make phpmd directory for output reports.

--- bin/test.sh
+++ bin/test.sh
@@ -1,2 +1,3 @@
 vendor/bin/phpunit -c ./ --coverage-html=$CIRCLE_ARTIFACTS/coverage --log-junit=$CIRCLE_TEST_REPORTS/junit.xml src/App/Test/*Test.php
+mkdir $CIRCLE_ARTIFACTS/phpmd
 vendor/bin/phpmd src html cleancode,codesize,controversial,design,naming,unusedcode --reportfile $CIRCLE_ARTIFACTS/phpmd/phpmd.html
