87cb036490c8c549926b63525a673d9c687abf7a
imura <imura@hitomedia.jp>
2015-02-23 17:19:16 +0900

    Modified test.sh

--- bin/test.sh
+++ bin/test.sh
@@ -1,6 +1,5 @@
-#!/bin/sh
-coverage-html=$CIRCLE_ARTIFACTS/coverage --log-junit=$CIRCLE_TEST_REPORTS/junit.xml src/App/Test/*Test.php
+#!/bin/sh -x
+vendor/bin/phpunit -c ./ --coverage-html=$CIRCLE_ARTIFACTS/coverage --log-junit=$CIRCLE_TEST_REPORTS/junit.xml src/App/Test/*Test.php
 rm -rf $CIRCLE_ARTIFACTS/phpmd
 mkdir $CIRCLE_ARTIFACTS/phpmd
-vendor/bin/phpmd src html cleancode,codesize,controversial,design,naming,unusedcode --reportfile $CIRCLE_ARTIFACTS/phpmd/phpmd.html >/dev/null 2>&1
-exit 0
+vendor/bin/phpmd src html cleancode,codesize,controversial,design,naming,unusedcode --reportfile $CIRCLE_ARTIFACTS/phpmd/phpmd.html | true
