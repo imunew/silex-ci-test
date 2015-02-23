fae9ac483b1ee3124b6d823130df58966772d3d0
imura <imura@hitomedia.jp>
2015-02-23 16:00:37 +0900

    Add phpmd report.

--- bin/test.sh
+++ bin/test.sh
@@ -1,1 +1,2 @@
 vendor/bin/phpunit -c ./ --coverage-html=$CIRCLE_ARTIFACTS/coverage --log-junit=$CIRCLE_TEST_REPORTS/junit.xml src/App/Test/*Test.php
+vendor/bin/phpmd src html cleancode,codesize,controversial,design,naming,unusedcode --reportfile $CIRCLE_ARTIFACTS/phpmd/phpmd.html
